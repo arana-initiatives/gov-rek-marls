d0ed2ea959d9441b8bbfade28e9df1416feac228
Ashish Rana <ashishrana160796@gmail.com>
2023-03-31 02:27:11 +0200

    HER goal env support added

--- README.md
+++ README.md
@@ -11,5 +11,7 @@ For debugging the project and parallely interacting with the codebase follow bel
 * First, open the project in your IDE and set the project `PYTHONPATH` 
 with command `export PYTHONPATH=${PYTHONPATH}:$/.`
 
-* Second, simply run the command `python **_**/**_**.py` to test specific module
-of your choice from the project while your `pwd` is `{some-base-path}/gov-rs-marls/`
+* Second, simply run the command in your `conda` environment `/home/{**path**}/miniconda3/envs/gov-rs-marls/bin/python
+/home/{**path**}/gov-rs-marls/govrs/envs/openai/road_env.py` to test specific module of your choice from the project
+while your `pwd` is `{some-base-path}/gov-rs-marls/`.
+  * **Note:** In the above `python` command, `gov-rs-marls` is also the name of the `conda` environment in use, same as the `pwd` or repository name.
--- govrs/envs/common/entities.py
+++ govrs/envs/common/entities.py
@@ -1,6 +1,7 @@
 import math
 import random
 import numpy as np
+from copy import deepcopy
 from abc import ABC, abstractmethod
 
 class Entity(ABC):
@@ -31,14 +32,23 @@ class World(Entity):
 class SimpleGridDroneWorld(World):
     def __init__(self, size, default_world = True, num_blockers = 0):
         super(SimpleGridDroneWorld, self).__init__(size, default_world, num_blockers)
-        self.world = np.zeros((self.size, self.size, self.size), dtype=int) # zyx indexing system
-        self.world = self.populate_world(self.world, self.size, self.default_world, self.num_blockers)
+        self.world = np.zeros((self.size, self.size, self.size), \
+                              dtype=int) # zyx indexing system
+        self.goal_world = np.zeros((self.size, self.size, self.size), \
+                              dtype=int) # zyx indexing system
+        
+        self.world = self.populate_world(self.world, self.size, \
+                                         self.default_world, self.num_blockers)
+        
+        self.goal_world = deepcopy(self.world)
+        self.goal_world = self.generate_goal_world(self.goal_world, self.size)
 
     def populate_world(self, world_arr, size, default_world, num_blockers):
         world_arr[0][0][0] = 1 # represents the first agent
         world_arr[size-1][size-1][size-1] = 4 # represents the final goal
         world_arr[0][size//2][size-1] = 2 # represents the second agent
         world_arr[0][size//2][0] = 3 # represents the package
+
         # masking the entire bottom field for drone movements
         floor_indices = np.where(world_arr[0,:,:] == 0)
         x_idx_flr, y_idx_flr = list(floor_indices[0]), list(floor_indices[1])
@@ -52,23 +62,46 @@ class SimpleGridDroneWorld(World):
             if num_blockers > math.sqrt(size*size*size):
                 num_blockers = int(math.sqrt(size*size*size))
             valid_indices = np.where(world_arr == 0)
-            x_idx, y_idx, z_idx = list(valid_indices[0]), list(valid_indices[1]), list(valid_indices[2])
+            z_idx, y_idx, x_idx = list(valid_indices[0]), list(valid_indices[1]), \
+                                    list(valid_indices[2])
             for i in range(num_blockers):
                 blocker_ind = random.choice(range(len(x_idx)))
-                world_arr[x_idx[blocker_ind]][y_idx[blocker_ind]][z_idx[blocker_ind]] = 5 # represents the blocker object
+                # representation of the blocker object
+                world_arr[z_idx[blocker_ind]][y_idx[blocker_ind]][x_idx[blocker_ind]] = 5
                 x_idx.pop(blocker_ind)
                 y_idx.pop(blocker_ind)
                 z_idx.pop(blocker_ind)
 
         return world_arr
     
+    def generate_goal_world(self, world_arr, size):
+        agent_two_indices = np.where(world_arr == 2)
+        world_arr[agent_two_indices[0][0]][agent_two_indices[1][0]] \
+            [agent_two_indices[2][0]] = 0
+
+        world_arr[size-1][size-1][size-1] = 2
+        agent_one_indices = np.where(world_arr == 1) 
+        package_indices = np.where(world_arr == 3)
+
+        world_arr[agent_one_indices[0][0]][agent_one_indices[1][0]] \
+            [agent_one_indices[2][0]] = 0
+        world_arr[package_indices[0][0]][package_indices[1][0]] \
+            [package_indices[2][0]] = 0
+
+        return world_arr
 
 class SimpleGridRoadWorld(World):
     def __init__(self, size, default_world = True, num_blockers = 0):
         super(SimpleGridRoadWorld, self).__init__(size, default_world, num_blockers)
         self.world = np.zeros((self.size, self.size), dtype=int)
-        self.world = self.populate_world(self.world, self.size, self.default_world, self.num_blockers)
-    
+        self.goal_world = np.zeros((self.size, self.size), dtype=int)
+
+        self.world = self.populate_world(self.world, self.size, \
+                                         self.default_world, self.num_blockers)
+        
+        self.goal_world = deepcopy(self.world)
+        self.goal_world = self.generate_goal_world(self.goal_world, self.size)
+
     def populate_world(self, world_arr, size, default_world, num_blockers):
         world_arr[0][0] = 1 # represents the first agent
         world_arr[size-1][size-1] = 4 # represents the final goal
@@ -87,5 +120,18 @@ class SimpleGridRoadWorld(World):
                 world_arr[y_idx[blocker_ind]][x_idx[blocker_ind]] = 5 # represents the blocker object
                 x_idx.pop(blocker_ind)
                 y_idx.pop(blocker_ind)
+
+        return world_arr
+
+    def generate_goal_world(self, world_arr, size):
+        agent_two_indices = np.where(world_arr == 2)
+        world_arr[agent_two_indices[0][0]][agent_two_indices[1][0]] = 0
+
+        world_arr[size-1][size-1] = 2
+        agent_one_indices = np.where(world_arr == 1) 
+        package_indices = np.where(world_arr == 3)
+
+        world_arr[agent_one_indices[0][0]][agent_one_indices[1][0]] = 0
+        world_arr[package_indices[0][0]][package_indices[1][0]] = 0
 
         return world_arr
